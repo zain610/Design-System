6a45a7010b6efe33beb507ac48e1288a8051d88d
Zain Shroff <zainshroff@macbook-pro.home>
2020-07-08 18:05:44 +1000

    learning to write SASS for nested classes and variable css

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,5 +1,5 @@
 import React from "react"
-import "./Button.css"
+import "./Button.scss"
 
 const ButtonComponent = ({ type = "default", size = "small", disableShadow = false }) => {
   //generate class name based on props
@@ -10,7 +10,9 @@ const ButtonComponent = ({ type = "default", size = "small", disableShadow = fal
   //btn-small disableShadow
   return (
     <div>
+      <button className="btn-small default">Default</button>
       <button className="btn-small default outline">Default</button>
+      <button className="btn-small default text">Default</button>
     </div >
   )
 }
