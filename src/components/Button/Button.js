c33fde31c6bd3b375bf904f75991840056071e2f
Zain Shroff <zainshroff@MacBook-Pro.local>
2020-07-10 20:44:25 +1000

    completed danger and success

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -26,7 +26,16 @@ const ButtonComponent = ({ type = "default", size = "small", disableShadow = fal
         <button className="btn-small secondary outline">Secondary</button>
         <button className="btn-small secondary text">Secondary</button>
       </div >
-      <button className="btn-small danger">Danger</button>
+      <div>
+        <button className="btn-small danger">Danger</button>
+        <button className="btn-small danger outline">Danger</button>
+        <button className="btn-small danger text">Danger</button>
+      </div>
+      <div>
+        <button className="btn-small success">Success</button>
+        <button className="btn-small success outline">Success</button>
+        <button className="btn-small success text">Success</button>
+      </div>
     </div>
   )
 }
