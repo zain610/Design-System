0665149fd83c141616fb75a9a088c31c205d67a3
zain610 <znshroff@gmail.com>
2020-07-11 22:22:18 +1000

    finalising display layout

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,33 @@ import Button from "./Button/Button"
 
 
 
+
 const App = () => {
   return (
     <div className="container">
-      <Button />
-      <Button color="success" type="outline" />
-      {/* <Button type="default" />
-      <Button type="outline" />
-      <Button type="text" /> */}
+      <div className="headers">
+        <h1>Buttons</h1>
+      </div>
+      <div className="buttons">
+        <Button color="default" type="default" text="Default" />
+        <Button color="primary" type="default" text="primary" />
+        <Button color="secondary" type="default" text="secondary" />
+        <Button color="success" type="default" text="Success" />
+        <Button color="danger" type="default" text="danger" />
+
+        <Button color="default" type="outline" text="Default Outline" />
+        <Button color="primary" type="outline" text="primary Outline" />
+        <Button color="secondary" type="outline" text="Secondary Outline" />
+        <Button color="success" type="outline" text="Success Outline" />
+        <Button color="danger" type="outline" text="Danger Outline" />
+
+        <Button color="default" type="text" text="Default Text" />
+        <Button color="primary" type="text" text="primary Text" />
+        <Button color="secondary" type="text" text="secondary Text" />
+        <Button color="success" type="text" text="Success Text" />
+        <Button color="danger" type="text" text="danger Text" />
+        <Button color="default" type="default" text="defualt disableShadow" disableShadow />
+      </div>
     </div>
   )
 }
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,19 +1,34 @@
 import React from "react"
 import "./Button.scss"
-
-const ButtonComponent = ({ color = "default", type = "default", size = "small", disableShadow = false, text = "Default" }) => {
+import PropTypes from 'prop-types'
+const ButtonComponent = ({ color, type, size, disableShadow, text }) => {
+  console.log(document.body.parentNode)
   //generate class name based on props
   const generateClassName = () => {
     let className = `btn-${size} ${type} ${color}`
     className += disableShadow ? " disableShadow" : ""
     return className
   }
-  console.log("class", generateClassName())
   //btn-small outline disbleShadow
   return (
-    <button className={generateClassName()}>{text}</button>
+    <div className="btn-div">
+      <button className={generateClassName()}>{text}</button>
+    </div>
 
   )
+}
+ButtonComponent.propTypes = {
+  color: PropTypes.string,
+  type: PropTypes.string,
+  size: PropTypes.string,
+  disableShadow: PropTypes.bool,
+  text: PropTypes.string.isRequired
+}
+ButtonComponent.defaultProps = {
+  color: "default",
+  type: "default",
+  size: "sm",
+  disableShadow: false
 }
 
 export default ButtonComponent
